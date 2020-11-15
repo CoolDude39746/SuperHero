7ac0a8248bbea0c8e793d9655a05daf011f5f27a
CoolDude39746 <71583552+CoolDude39746@users.noreply.github.com>
2020-11-15 11:10:28 +0530

    Add files via upload

--- Main.js
+++ Main.js
@@ -8,7 +8,7 @@ var PlayerObject="";
 var ImageObject="";
 
 function PlayerUpdate() {
-    fabric.Image.fromURL(""),function(Img) {
+    fabric.Image.fromURL("Final_output.png"),function(Img) {
         PlayerObject=Img;
         PlayerObject.scaleToWidth(150);
         PlayerObject.scaleToHeight(140);
@@ -17,7 +17,7 @@ function PlayerUpdate() {
             left:PlayerX,
         });
         Canvas.add(PlayerObject);
-    })
+    }
 }
 
 function NewImage(get_image) {
@@ -31,4 +31,68 @@ function NewImage(get_image) {
         });
         Canvas.add(ImageObject);
     })
+}
+
+function myKeyDown(e) {
+    KeyPressed=e.keyCode;
+    console.log(KeyPressed);
+    if (e.shiftKey==true && KeyPressed=='80') {
+        console.log("p and shift");
+        block_imageWidth=block_imageWidth+10;
+        block_imageHeight=block_imageHeight+10;
+        document.getElementById("current_width").innerHTML=block_imageWidth;
+        document.getElementById("current_height").innerHTML=block_imageHeight;
+    }
+
+    if (e.shiftKey==true && KeyPressed=='77') {
+        console.log("m and shift");
+        block_imageWidth=block_imageWidth-10;
+        block_imageHeight=block_imageHeight-10;
+        document.getElementById("current_width").innerHTML=block_imageWidth;
+        document.getElementById("current_height").innerHTML=block_imageHeight;
+    }
+
+    if (KeyPressed=='38') {
+        Up();
+        console.log("Up");
+    }
+
+    function Up() {
+    if (PlayerY >= 20) {
+        PlayerY = PlayerY-block_imageHeight;
+        console.log("block_imageHeight" + block_imageHeight);
+        console.log("When Up Arrow Is Pressed,X =" + PlayerX + ",Y =" + PlayerY);
+        Canvas.remove(PlayerObject);
+        PlayerUpdate();
+    }
+}
+
+function Down() {
+    if (PlayerY <= 450) {
+        PlayerY = PlayerY+block_imageHeight;
+        console.log("block_imageHeight" + block_imageHeight);
+        console.log("When Down Arrow Is Pressed,X =" + PlayerX + ",Y =" + PlayerY);
+        Canvas.remove(PlayerObject);
+        PlayerUpdate();
+    }
+}
+
+function Left() {
+    if (PlayerX >= 50) {
+        PlayerX = PlayerX-block_imageWidth;
+        console.log("block_imageWidth" + block_imageWidth);
+        console.log("When Left Arrow Is Pressed,X =" + PlayerX + ",Y =" + PlayerY);
+        Canvas.remove(PlayerObject);
+        PlayerUpdate();
+    }
+}
+
+function Right() {
+    if (PlayerX <= 900) {
+        PlayerX = PlayerX+block_imageWidth;
+        console.log("block_imageWidth" + block_imageWidth);
+        console.log("When Right Arrow Is Pressed,X =" + PlayerX + ",Y =" + PlayerY);
+        Canvas.remove(PlayerObject);
+        PlayerUpdate();
+    }
 }
